952e561f77dd0ec39927d53896b390dc2c95f430
Zhou You <yollyhideout@gmail.com>
2016-05-19 21:16:55 +0800

    add production build

--- src/all.js
+++ src/all.js
@@ -50,7 +50,9 @@
 	var ReactDOM = __webpack_require__(96);
 	var App = __webpack_require__(93);
 	
-	ReactDOM.render(React.createElement(App, null), document.querySelector('#app'));
+	var grids = [{ color: '#456990', name: 'dark blue' }, { color: '#EF767A', name: 'red' }, { color: '#49BEAA', name: 'dark green' }, { color: '#49DCB1', name: 'light green' }, { color: '#EEB868', name: 'yellow' }, { color: '#EF767A', name: 'red' }, { color: '#456990', name: 'dark blue' }, { color: '#49BEAA', name: 'dark green' }, { color: '#49DCB1', name: 'green' }, { color: '#EEB868', name: 'yellow' }, { color: '#EF767A', name: 'red' }];
+	
+	ReactDOM.render(React.createElement(App, { grids: grids }), document.querySelector('#app'));
 
 /***/ },
 /* 1 */
@@ -10291,51 +10293,82 @@
 	var Motion = __webpack_require__(59).Motion;
 	var spring = __webpack_require__(59).spring;
 	
-	var allColors = ['#456990', '#EF767A', '#49BEAA', '#49DCB1', '#EEB868', '#EF767A', '#456990', '#49BEAA', '#49DCB1', '#EEB868', '#EF767A'];
-	
 	var springSetting1 = { stiffness: 180, damping: 10 };
 	var springSetting2 = { stiffness: 120, damping: 17 };
-	
-	var names = ['dark blue', 'red', 'dark green', 'light green', 'yellow', 'red', 'dark blue', 'dark green', 'green', 'yellow', 'red'];
-	
-	var count = 11;
-	var width = 70;
-	var height = 90;
-	
-	var layout = range(count).map(function (n) {
-		var row = Math.floor(n / 3);
-		var col = n % 3;
-		return [width * col, height * row];
-	});
-	var reinsert = function reinsert(oldOrder, value) {
-		var grids = oldOrder.filter((function (_, key) {
-			if (value === '') {
-				return true;
-			} else {
-				return names[key].indexOf(value) >= 0;
-			}
-		}).bind(this));
-		return grids;
-	};
 	
 	var App = React.createClass({
 		displayName: 'App',
 	
 		getInitialState: function getInitialState() {
 			return {
-				order: range(count),
-				filteredOrder: range(count),
-				value: ''
+				order: range(this.props.grids.length),
+				filteredOrder: range(this.props.grids.length),
+				value: '',
+				grids: this.props.grids
 			};
 		},
+		_reinsert: function _reinsert(oldOrder, value) {
+			return oldOrder.filter((function (_, key) {
+				if (value === '') {
+					return true;
+				} else {
+					return this.state.grids[key].name.indexOf(value) >= 0;
+				}
+			}).bind(this));
+		},
+	
 		handleChange: function handleChange(e) {
 			var value = e.target.value;
-			var newOrder = reinsert(this.state.order, value);
+			var newOrder = this._reinsert(this.state.order, value);
 			this.setState({
 				value: value,
 				filteredOrder: newOrder
 			});
 		},
+	
+		handleRemove: function handleRemove(key) {
+			return (function () {
+				var newGirds = key === 0 ? this.state.grids.slice(1) : key === this.state.grids.length - 1 ? this.state.grids.slice(0, key + 1) : this.state.grids.slice(0, key).concat(this.state.grids.slice(key + 1));
+				this.setState({
+					grids: newGirds,
+					order: range(newGirds.length - 1)
+				});
+			}).bind(this);
+		},
+	
+		_getStyles: function _getStyles(index) {
+			var style = {};
+			var count = this.state.grids.length;
+			var width = 70;
+			var height = 90;
+			var column = 3;
+			var layout = range(this.state.grids.length).map(function (n) {
+				var row = Math.floor(n / column);
+				var col = n % column;
+				return [width * col, height * row];
+			});
+			if (this.state.filteredOrder.indexOf(index) < 0) {
+				var visualPosition = this.state.order.indexOf(index);
+				var x = layout[visualPosition][0];
+				var y = layout[visualPosition][1];
+				style = {
+					scale: spring(0, springSetting2),
+					translateX: x,
+					translateY: y
+				};
+			} else {
+				var visualPosition = this.state.filteredOrder.indexOf(index);
+				var x = layout[visualPosition][0];
+				var y = layout[visualPosition][1];
+				style = {
+					scale: spring(1, springSetting2),
+					translateX: spring(x, springSetting2),
+					translateY: spring(y, springSetting2)
+				};
+			}
+			return style;
+		},
+	
 		render: function render() {
 			return React.createElement(
 				'div',
@@ -10346,7 +10379,7 @@
 					React.createElement('input', {
 						className: 'filter-input',
 						autoFocus: false,
-						placeholder: 'filter out cards',
+						placeholder: 'Type a color!',
 						onChange: this.handleChange
 					})
 				),
@@ -10354,43 +10387,24 @@
 					'div',
 					{ className: 'grids' },
 					this.state.order.map((function (index, key) {
-						var style = {};
-						if (this.state.filteredOrder.indexOf(index) < 0) {
-							var visualPosition = this.state.order.indexOf(index);
-							var x = layout[visualPosition][0];
-							var y = layout[visualPosition][1];
-							style = {
-								scale: spring(0, springSetting2),
-								translateX: x,
-								translateY: y
-							};
-						} else {
-							var visualPosition = this.state.filteredOrder.indexOf(index);
-							var x = layout[visualPosition][0];
-							var y = layout[visualPosition][1];
-							style = {
-								scale: spring(1, springSetting2),
-								translateX: spring(x, springSetting2),
-								translateY: spring(y, springSetting2)
-							};
-						}
 						return React.createElement(
 							Motion,
-							{ key: key, style: style },
-							function (style) {
+							{ key: key, style: this._getStyles(index) },
+							(function (style) {
 								var scale = style.scale;
 								var translateX = style.translateX;
 								var translateY = style.translateY;
 								return React.createElement(ExpandableDiv, {
 									key: key,
-									names: names,
+									name: this.state.grids[key].name,
 									style: {
-										backgroundColor: allColors[index],
+										backgroundColor: this.state.grids[index].color,
 										WebkitTransform: 'translate3d(' + translateX + 'px, ' + translateY + 'px, 0) scale(' + scale + ')',
 										transform: 'translate3d(' + translateX + 'px, ' + translateY + 'px, 0) scale(' + scale + ')'
-									}
+									},
+									handleRemove: this.handleRemove(key)
 								});
-							}
+							}).bind(this)
 						);
 					}).bind(this))
 				)
@@ -10416,13 +10430,13 @@
 				"div",
 				{
 					className: "ball",
-					key: this.props.key,
-					style: this.props.style
+					style: this.props.style,
+					onClick: this.props.handleRemove
 				},
 				React.createElement(
 					"span",
 					{ className: "number", style: { visibility: 'hidden' } },
-					this.props.names[this.props.key]
+					this.props.name
 				)
 			);
 		}
